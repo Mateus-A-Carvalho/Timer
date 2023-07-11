f1604e975884a80abbf34a12668cd8016963a129
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-07-10 21:06:28 -0300

    Making some changes on styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,10 @@ const buttonStart = document.querySelector('.start');
 const buttonPause = document.querySelector('.pause'); 
 const buttonRestart = document.querySelector('.restart'); 
 
+let seconds = 0;
 
-function getTimeInSeconds(seconds) { // This function has the purpose of transform milliseconds in seconds
+// Formating Time
+function getTimeInSeconds(seconds) { 
     const date = new Date(seconds * 1000);
     return date.toLocaleTimeString('pt-BR', {
         hour12: false,
@@ -12,15 +14,14 @@ function getTimeInSeconds(seconds) { // This function has the purpose of transfo
     })
 }
 
-function startTheTimer() { // This function start the clock. 
-    timer = setInterval(function() { // Set this method(setInterval) to a const.
-    seconds++; // Incrementing the variable that salve the value of seconds. 
-    clock.innerHTML = getTimeInSeconds(seconds); // Adding in the HTML content the function that format milliseconds in seconds.
-}, 1000); // Time that function will be executed.
+function startTheTimer() { 
+    timer = setInterval(() => {
+    seconds++;
+    clock.innerHTML = getTimeInSeconds(seconds); 
+}, 1000); 
 }
 
 
-// This way to code the same task is quicker and efficiant
 document.addEventListener('click', function(event) {
     const element = event.target;
 
@@ -42,34 +43,15 @@ document.addEventListener('click', function(event) {
         clock.innerHTML = `00:00:00`;
     }
 })
-// This way to code the same task is quicker and more efficient
 
 
 // Using closure to take this variables in these functions
-let seconds = 0;
+
 let timer;
 // Using closure to take this variables in these functions
 
 
-// ----- THIS AREA ARE ANOTHER AND LARGER SOLUTION ------ 
 
 
 
-// buttonStart.addEventListener('click', function(event) { // Starting the time
-//     clock.classList.remove('paused');
-//     clearInterval(timer);
-//     startTheTimer();
-// });
-
-// buttonPause.addEventListener('click', function(event) {
-//     clock.classList.add('paused');
-//     clearInterval(timer);
-// });
-
-// buttonRestart.addEventListener('click', function(event) {
-//     clock.classList.remove('paused');
-//     clearInterval(timer);
-//     seconds = 0;
-//     clock.innerHTML = `00:00:00`;
-// });
 
